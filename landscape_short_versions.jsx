0f17c6cd739b3a2f74e76ae7ee87803b77d09a61
marianina8 <mmontagnino@gmail.com>
2015-11-24 10:44:15 -0800

    Updated Comments

--- landscape_short_versions.jsx
+++ landscape_short_versions.jsx
@@ -1,66 +1,52 @@
-	// Photoshop Script to Create iPhone Icons from PortraitArtwork
-	//
-	// WARNING!!! In the rare case that there are name collisions, this script will
-	// overwrite (delete perminently) files in the same folder in which the selected
-	// PortraitArtwork file is located. Therefore, to be safe, before running the
-	// script, it's best to make sure the selected iTuensArtwork file is the only
-	// file in its containing folder.
-	//
-	// Copyright (c) 2010 Matt Di Pasquale
-	// Added tweaks Copyright (c) 2012 by Josh Jones http://www.appsbynight.com
-	//
-	// Permission is hereby granted, free of charge, to any person obtaining a copy
-	// of this software and associated documentation files (the "Software"), to deal
-	// in the Software without restriction, including without limitation the rights
-	// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-	// copies of the Software, and to permit persons to whom the Software is
-	// furnished to do so, subject to the following conditions:
-	//
-	// The above copyright notice and this permission notice shall be included in
-	// all copies or substantial portions of the Software.
-	//
-	// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-	// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-	// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-	// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-	// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-	// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-	// THE SOFTWARE.
-	//
-	// Prerequisite:
-	// First, create at least a 1024x1024 px PNG file according to:
-	// http://developer.apple.com/library/ios/#documentation/iphone/conceptual/iphoneosprogrammingguide/BuildTimeConfiguration/BuildTimeConfiguration.html
-	//
-	// Install - Save Create Icons.jsx to:
-	//   Win: C:\Program Files\Adobe\Adobe Utilities\ExtendScript Toolkit CS5\SDK
-	//   Mac: /Applications/Utilities/Adobe Utilities/ExtendScript Toolkit CS5/SDK
-	// * Restart Photoshop
-	//
-	// Update:
-	// * Just modify & save, no need to resart Photoshop once it's installed.
-	//
-	// Run:
-	// * With Photoshop open, select File > Scripts > Create Icons
-	// * When prompted select the prepared PortraitArtwork file for your app.
-	// * The different version of the versions will get saved to the same folder that
-	//   the PortraitArtwork file is in.
-	//
-	// Adobe Photoshop JavaScript Reference
-	// http://www.adobe.com/devnet/photoshop/scripting.html
+// Photoshop Script to Create Landscape Short Version VOD Artwork for DirecTV, DISH, CMC, and Neulion
+//
+// Author: Marian Montagnino
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+// Prerequisite:
+// First, create a 2048x768 px JPG file
+//
+// Install - Presets/Scripts/ folder
+// * Restart Photoshop
+//
+// Update:
+// * Just modify & save, no need to restart Photoshop once it's installed.
+//
+// Run:
+// * With Photoshop open, select File > Scripts > Landscape Short Versions
+// * When prompted select the prepared artwork file for your app.
+// * The different version of the versions will get saved to the selected output folder.
+//
+// Adobe Photoshop JavaScript Reference
+// http://www.adobe.com/devnet/photoshop/scripting.html
 
 
-	// Turn debugger on. 0 is off.
-	// $.level = 1;
+// Turn debugger on. 0 is off.
+// $.level = 1;
 
 	try
 	{
 	  // Prompt user to select PortraitArtwork file. Clicking "Cancel" returns null.
 	  var PortraitArtwork = File.openDialog("Select a JPG file that is at least 2048x768.", "*.jpg", false);
 
-	  if (PortraitArtwork !== null) 
-	  { 
+	  if (PortraitArtwork !== null)
+	  {
 	    var doc = open(PortraitArtwork, OpenDocumentType.JPEG);
-    
+
 	    if (doc == null)
 	    {
 	      throw "Something is wrong with the file.  Make sure it's a valid JPG file.";
@@ -73,10 +59,10 @@
 	    var startDisplayDialogs = app.displayDialogs
 	    // Set Adobe Photoshop CC 2015 to use pixels and display no dialogs
 	    app.displayDialogs = DialogModes.NO
-		
+
 	    app.preferences.rulerUnits = Units.PIXELS;     // use pixels
 		app.preferences.typeUnits = TypeUnits.PIXELS; //use pixels for type also
-		
+
 
 	    if ((doc.width.value < 2048) && (doc.height.value < 768))
 	    {
@@ -90,7 +76,7 @@
 	    {
 	        throw "Image height is too small!  Image height must be at least 1024 pixels.";
 	    }
-    
+
 	    // Folder selection dialog
 	    var destFolder = Folder.selectDialog( "Choose an output folder");
 
@@ -103,10 +89,10 @@
 	    var filename=prompt("Please enter title code:","DFF010017");
 	    // Save versions in PNG using Save for Web.
 	    var sfw = new ExportOptionsSaveForWeb();
-		sfw.quality = 60; 
-		sfw.includeProfile = true; 
-		sfw.optimised = true; 
-		sfw.format = SaveDocumentType.JPEG;  
+		sfw.quality = 60;
+		sfw.includeProfile = true;
+		sfw.optimised = true;
+		sfw.format = SaveDocumentType.JPEG;
 
 		doc.resizeCanvas(2048,550,AnchorPosition.MIDDLECENTER);
 		doc.resizeImage(1600, 430, null, ResampleMethod.BICUBICSHARPER);
@@ -135,7 +121,7 @@
 		doc.resizeImage(2048,768,null, ResampleMethod.BICUBICSHARPER);
 		var destFileName = filename + "_Hulu_2048x768.jpg";
 		doc.exportDocument(new File(destFolder + "/" + destFileName), ExportType.SAVEFORWEB, sfw);
-		doc.activeHistoryState = startState; // undo resize		  
+		doc.activeHistoryState = startState; // undo resize
 
 		doc.resizeCanvas(2048,567,AnchorPosition.MIDDLECENTER);
 		doc.resizeImage(1280,370,null, ResampleMethod.BICUBICSHARPER);
@@ -147,10 +133,10 @@
 		var destFileName = filename + "_Neulion_DynamicLead_2048x740.jpg";
 		doc.exportDocument(new File(destFolder + "/" + destFileName), ExportType.SAVEFORWEB, sfw);
 		doc.activeHistoryState = startState; // undo resize
-		
+
 		alert("Landscape (Short) Versions created!");
-		
-		
+
+
 	  }
 	}
 	catch (exception)
@@ -163,6 +149,6 @@
 	{
 	    if (doc != null)
 	        doc.close(SaveOptions.DONOTSAVECHANGES);
-  
+
 	    app.preferences.rulerUnits = initialPrefs; // restore prefs
 	}
--- square_versions.jsx
+++ square_versions.jsx
@@ -1,13 +1,6 @@
-	// Photoshop Script to Create iPhone Icons from PortraitArtwork
+	// Photoshop Script to Create Square Version VOD Artwork for DirecTV, DISH, CMC, and Neulion
 	//
-	// WARNING!!! In the rare case that there are name collisions, this script will
-	// overwrite (delete perminently) files in the same folder in which the selected
-	// PortraitArtwork file is located. Therefore, to be safe, before running the
-	// script, it's best to make sure the selected iTuensArtwork file is the only
-	// file in its containing folder.
-	//
-	// Copyright (c) 2010 Matt Di Pasquale
-	// Added tweaks Copyright (c) 2012 by Josh Jones http://www.appsbynight.com
+	// Author: Marian Montagnino
 	//
 	// Permission is hereby granted, free of charge, to any person obtaining a copy
 	// of this software and associated documentation files (the "Software"), to deal
@@ -15,9 +8,6 @@
 	// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 	// copies of the Software, and to permit persons to whom the Software is
 	// furnished to do so, subject to the following conditions:
-	//
-	// The above copyright notice and this permission notice shall be included in
-	// all copies or substantial portions of the Software.
 	//
 	// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 	// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -28,22 +18,18 @@
 	// THE SOFTWARE.
 	//
 	// Prerequisite:
-	// First, create at least a 1024x1024 px PNG file according to:
-	// http://developer.apple.com/library/ios/#documentation/iphone/conceptual/iphoneosprogrammingguide/BuildTimeConfiguration/BuildTimeConfiguration.html
+	// First, create a 2400x2400 px JPG file
 	//
-	// Install - Save Create Icons.jsx to:
-	//   Win: C:\Program Files\Adobe\Adobe Utilities\ExtendScript Toolkit CS5\SDK
-	//   Mac: /Applications/Utilities/Adobe Utilities/ExtendScript Toolkit CS5/SDK
+	// Install - Presets/Scripts/ folder
 	// * Restart Photoshop
 	//
 	// Update:
-	// * Just modify & save, no need to resart Photoshop once it's installed.
+	// * Just modify & save, no need to restart Photoshop once it's installed.
 	//
 	// Run:
-	// * With Photoshop open, select File > Scripts > Create Icons
-	// * When prompted select the prepared PortraitArtwork file for your app.
-	// * The different version of the versions will get saved to the same folder that
-	//   the PortraitArtwork file is in.
+	// * With Photoshop open, select File > Scripts > Square Versions
+	// * When prompted select the prepared artwork file for your app.
+	// * The different version of the versions will get saved to the selected output folder.
 	//
 	// Adobe Photoshop JavaScript Reference
 	// http://www.adobe.com/devnet/photoshop/scripting.html
@@ -57,11 +43,11 @@
 	  // Prompt user to select PortraitArtwork file. Clicking "Cancel" returns null.
 	  var PortraitArtwork = File.openDialog("Select a JPG file that is at least 2400x2400.", "*.jpg", false);
 
-	  if (PortraitArtwork !== null) 
-	  { 
-		
+	  if (PortraitArtwork !== null)
+	  {
+
 	    var doc = open(PortraitArtwork, OpenDocumentType.JPEG);
-    
+
 	    if (doc == null)
 	    {
 	      throw "Something is wrong with the file.  Make sure it's a valid JPG file.";
@@ -73,10 +59,10 @@
 	    var startDisplayDialogs = app.displayDialogs
 	    // Set Adobe Photoshop CC 2015 to use pixels and display no dialogs
 	    app.displayDialogs = DialogModes.NO
-		
+
 	    app.preferences.rulerUnits = Units.PIXELS;     // use pixels
 		app.preferences.typeUnits = TypeUnits.PIXELS; //use pixels for type also
-		
+
 		if (doc.width.value != doc.height.value)
 		{
 			throw "Image is not a square.";
@@ -93,7 +79,7 @@
 	    {
 	        throw "Image height is too small!  Image height must be at least 1024 pixels.";
 	    }
-    
+
 	    // Folder selection dialog
 	    var destFolder = Folder.selectDialog( "Choose an output folder");
 
@@ -106,13 +92,13 @@
 	    var filename=prompt("Please enter title code:","DFF010017");
 	    // Save versions in PNG using Save for Web.
 	    var sfw = new ExportOptionsSaveForWeb();
-		sfw.quality = 60; 
-		sfw.includeProfile = true; 
-		sfw.optimised = true; 
-		sfw.format = SaveDocumentType.JPEG;  
-		
+		sfw.quality = 60;
+		sfw.includeProfile = true;
+		sfw.optimised = true;
+		sfw.format = SaveDocumentType.JPEG;
+
 	    var sfw_bmp = new ExportOptionsSaveForWeb();
-		sfw_bmp.format = SaveDocumentType.BMP;  		
+		sfw_bmp.format = SaveDocumentType.BMP;
 
 		doc.resizeImage(2400, 2400, null, ResampleMethod.BICUBICSHARPER);
 		var destFileName = filename + "_iTunes_2400x2400.jpg";
@@ -128,7 +114,7 @@
 		var destFileName = filename + "_Neulion_160x160.jpg";
 		doc.exportDocument(new File(destFolder + "/" + destFileName), ExportType.SAVEFORWEB, sfw);
 		doc.activeHistoryState = startState; // undo resize
-		
+
 		doc.resizeImage(1080, 1080, null, ResampleMethod.BICUBICSHARPER);
 		var destFileName = filename + "_Verizon_1080x1080.jpg";
 		doc.exportDocument(new File(destFolder + "/" + destFileName), ExportType.SAVEFORWEB, sfw);
@@ -139,33 +125,33 @@
 		var destFileName = filename + "_CMC_320x250.bmp";
 		doc.exportDocument(new File(destFolder + "/" + destFileName), ExportType.SAVEFORWEB, sfw_bmp);
 		doc.activeHistoryState = startState; // undo resize
-		
+
 		doc.resizeCanvas(2024,2400,AnchorPosition.MIDDLECENTER);
 		doc.resizeImage(430, 510, null, ResampleMethod.BICUBICSHARPER);
 		var destFileName = filename + "_Neulion_430x510.jpg";
 		doc.exportDocument(new File(destFolder + "/" + destFileName), ExportType.SAVEFORWEB, sfw);
 		doc.activeHistoryState = startState; // undo resize
-		
+
 		doc.resizeCanvas(2400,1800,AnchorPosition.MIDDLECENTER);
 		doc.resizeImage(2048, 1536, null, ResampleMethod.BICUBICSHARPER);
 		var destFileName = filename + "_Rovi_2048x1536.jpg";
 		doc.exportDocument(new File(destFolder + "/" + destFileName), ExportType.SAVEFORWEB, sfw);
 		doc.activeHistoryState = startState; // undo resize
-		
-		doc.resizeCanvas(2400,1800,AnchorPosition.MIDDLECENTER);	  
+
+		doc.resizeCanvas(2400,1800,AnchorPosition.MIDDLECENTER);
 		doc.resizeImage(1440, 1080, null, ResampleMethod.BICUBICSHARPER);
 		var destFileName = filename + "_Sony_1440x1080.jpg";
 		doc.exportDocument(new File(destFolder + "/" + destFileName), ExportType.SAVEFORWEB, sfw);
 		doc.activeHistoryState = startState; // undo resize
-		
-		doc.resizeCanvas(2400,1800,AnchorPosition.MIDDLECENTER);		
+
+		doc.resizeCanvas(2400,1800,AnchorPosition.MIDDLECENTER);
 		doc.resizeImage(1440, 1080, null, ResampleMethod.BICUBICSHARPER);
 		var destFileName = filename + "_Verizon_1440x1080.jpg";
 		doc.exportDocument(new File(destFolder + "/" + destFileName), ExportType.SAVEFORWEB, sfw);
 		doc.activeHistoryState = startState; // undo resize
-  
+
 		alert("Square Versions created!");
-		
+
 	  }
 	}
 	catch (exception)
@@ -178,6 +164,6 @@
 	{
 	    if (doc != null)
 	        doc.close(SaveOptions.DONOTSAVECHANGES);
-  
+
 	    app.preferences.rulerUnits = initialPrefs; // restore prefs
 	}
